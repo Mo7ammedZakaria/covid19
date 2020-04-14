60675c05ad64e0b8fff7d0edda48ea20d01317f9
Mohammed Zakaria <web.dev@armatx.com>
2020-04-14 12:37:10 +0300

    add the function for x and y axis relabling

--- javascript/app.js
+++ javascript/app.js
@@ -1,44 +1,41 @@
 
 // https://api.covid19api.com/
 
-
-
-//  fetch('https://api.covid19api.com/dayone/country/turkey') .then(response => response.json()).then(data =>{
-// console.log(data);
-// });
-
 fetch("https://api.covid19api.com/dayone/country/turkey")
     .then(response => response.json())
     .then(data => {
         const dataArray = data;
-        console.log(data);
+        // extract the confirmed numbers to array
         const confirmedArray = dataArray.map((element) => {
             return (element.Confirmed);
         });
 
         var xAccessLine = 1;
-        // console.log();
         var arrayOfDailyConfirmedNumbers = [];
         const xArray = [];
         const yArray = [];
+        // initial the first point at the graph
+        xArray.push(confirmedArray[0]);
+        yArray.push(0);
+
+        //filter the cases to output the cases for each day
         for (let index = 0; index < confirmedArray.length; index++) {
             if (confirmedArray[index] !== confirmedArray[index - 1]) {
                 arrayOfDailyConfirmedNumbers.push(confirmedArray[index] - confirmedArray[index - 1]);
             }
         }
 
-
+        // loop to push data to the x and y axis
         for (let index = 0; index < arrayOfDailyConfirmedNumbers.length; index++) {
             if (arrayOfDailyConfirmedNumbers[index] > 0) {
                 const element = arrayOfDailyConfirmedNumbers[index];
-                xAccessLine = xAccessLine;
+                xAccessLine = xAccessLine + element;
                 xArray.push(xAccessLine);
                 yArray.push(element);
             }
         }
 
-        const array = [1, 4, 187, 167, 311, 566];
-
+        var apexYAxisValue = 10;
         const options = {
             chart: {
                 height: 450,
@@ -53,67 +50,51 @@ fetch("https://api.covid19api.com/dayone/country/turkey")
                     data: yArray,
                 },
             ],
-            yaxis: {
-                show: true,
-                showAlways: true,
-                showForNullSeries: true,
-                seriesName: undefined,
-                opposite: false,
-                reversed: false,
-                logarithmic: false,
-                tickAmount: 6,
-                min: 6,
-                max: 6,
-                forceNiceScale: false,
-                floating: false,
-                decimalsInFloat: undefined,
-                labels: {
-                    show: true,
-                    align: 'right',
-                    minWidth: 0,
-                    maxWidth: 160,
-                    style: {
-                        colors: [],
-                        fontSize: '12px',
-                        fontFamily: 'Helvetica, Arial, sans-serif',
-                        fontWeight: 400,
-                        cssClass: 'apexcharts-yaxis-label',
-                    },
-                    offsetX: 0,
-                    offsetY: 0,
-                    rotate: 0,
-                    formatter: (value) => { return val },
-                },
-                axisTicks: {
-                    show: true,
-                    borderType: 'solid',
-                    color: '#78909C',
-                    width: 6,
-                    offsetX: 0,
-                    offsetY: 0
-                },
-                title: {
-                    text: yArray,
-                    rotate: -90,
-                    offsetX: 0,
-                    offsetY: 0,
-                    style: {
-                        color: 'white',
-                        fontSize: '12px',
-                        fontFamily: 'Helvetica, Arial, sans-serif',
-                        fontWeight: 600,
-                        cssClass: 'apexcharts-yaxis-title',
-                    },
-                },
-                tooltip: {
-                    enabled: true,
-                    offsetX: 0,
-                },
-                
-            },
             xaxis: {
-                categories: xArray
+                categories: xArray,
+                // labels: {
+                //     show: true,
+                //     align: 'right',
+                //     minWidth: 0,
+                //     maxWidth: 160,
+                //     style: {
+                //         colors: [],
+                //         fontSize: '12px',
+                //         fontFamily: 'Helvetica, Arial, sans-serif',
+                //         fontWeight: 400,
+                //         cssClass: 'apexcharts-yaxis-label',
+                //     },
+                //     offsetX: 0,
+                //     offsetY: 0,
+                //     rotate: 0,
+                //     formatter: () => { 
+                //         apexYAxisValue = apexYAxisValue * 10;
+                //         return apexYAxisValue 
+                //     },
+                // },
             },
+            // yaxis: {
+            //     labels: {
+            //         show: true,
+            //         align: 'right',
+            //         minWidth: 0,
+            //         maxWidth: 160,
+            //         style: {
+            //             colors: [],
+            //             fontSize: '12px',
+            //             fontFamily: 'Helvetica, Arial, sans-serif',
+            //             fontWeight: 400,
+            //             cssClass: 'apexcharts-yaxis-label',
+            //         },
+            //         offsetX: 0,
+            //         offsetY: 0,
+            //         rotate: 0,
+            //         formatter: () => { 
+            //             apexYAxisValue = apexYAxisValue * 10;
+            //             return apexYAxisValue 
+            //         },
+            //     },
+            // },
             plotOptions: {
                 bar: {
                     horizontal: false
@@ -138,9 +119,8 @@ fetch("https://api.covid19api.com/dayone/country/turkey")
         };
         // init chart
         const chartDiv = document.querySelector("#chart");
-        // console.log(chartDiv);
-        const chart = new ApexCharts(document.querySelector("#chart"), options);
+        // add the chart to the html tag
+        const chart = new ApexCharts(chartDiv, options);
         // render chart
-        // console.log(chart);
         chart.render();
     });
